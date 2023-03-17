f0cd7956a9e8fe10beb7ed9f1ada8f7cc42a1215
Blibox <95865510+Blibox@users.noreply.github.com>
2023-03-17 09:28:09 +0100

    ajout de fonctions api_utils.php

--- api/authentifier/index.php
+++ api/authentifier/index.php
@@ -4,18 +4,16 @@ require('../files/jwt_utils.php');
 require('../conn.php');
 require('../files/api-utils.php');
 header("Content-Type:application/json");
-/// Identification du type de méthode HTTP envoyée par le client
 $http_method = $_SERVER['REQUEST_METHOD'];
-// Check if the request is a POST
+
 if ($http_method != 'POST') {
-    // If not a POST, then send a 405 Method Not Allowed response
-    header("HTTP/1.1 405 Method Not Allowed");
+    deliver_response(405, "Method Not Allowed", NULL);
     exit();
 }
-// Get the JSON data
 $data = json_decode(file_get_contents('php://input'), true);
-// Check if the JSON contain the login and mdp fields
 if (!isset($data['login']) || !isset($data['mdp'])) {
-    // If not, then send a 400 Bad Request response
+    deliver_response(400, "Bad Request, POST body must contain login & mdp fields", NULL);
     exit();
 }
+$login = $data['login'];
+$mdp = $data['mdp'];
--- api/files/api_utils.php
+++ api/files/api_utils.php
@@ -1,5 +1,5 @@
 <?php
-function deliver_response($status, $status_message, $data)
+function deliver_response(int $status, string $status_message, $data): void
 {
 
     $response = array();
@@ -9,6 +9,54 @@ function deliver_response($status, $status_message, $data)
     $response['status_message'] = $status_message;
     $response['data'] = $data;
     $json_response = json_encode($response);
-    // var_dump($json_response);
     echo $json_response;
+}
+/**
+ * Function is_authorized
+ * Check if the user is authorized to access the resource he is trying to access
+ * If the user is not authorized, the function will deliver a response and return false
+ * @param int $privileges_required : the privileges required to access the resource
+ * @return bool : true if the user is authorized, false otherwise
+ **/
+function is_authorized(int $privileges_required): bool
+{
+    require('./jwt_utils.php');
+    $jwt = get_bearer_token();
+    if ($jwt == NULL && $privileges_required == 0) {
+        return true;
+    } else if ($jwt == NULL && $privileges_required != 0) {
+        deliver_response(401, "Unauthorized, unable to locate your authentification token", NULL);
+        return false;
+    }
+    if (is_jwt_valid($jwt)) {
+        $payload = json_decode(base64_decode(explode('.', $jwt)[1]));
+        if ($payload->privileges >= $privileges_required) {
+            return true;
+        } else {
+            deliver_response(403, "Forbidden", NULL);
+            return false;
+        }
+    } else {
+        deliver_response(401, "Unauthorized, invalid token", NULL);
+        return false;
+    }
+}
+/**
+ * Function is_valid_user
+ * @param string $login : the login of the user
+ * @param string $mdp : the password of the user
+ * @return bool|int : false if the user is not valid, the role of the user if it is valid
+ */
+function is_valid_user(string $login, string $mdp): bool|int
+{
+    require('./bdd_utils.php');
+    $conn = DBConnection::getInstance()->getConnection();
+    $sql = "SELECT role FROM users WHERE login = '$login' AND mdp = '$mdp'";
+    $result = $conn->query($sql);
+    if ($result->rowCount() == 1) {
+        $privilege = $result->fetch(PDO::FETCH_ASSOC)['role'];
+        return $privilege;
+    } else {
+        return false;
+    }
 }
